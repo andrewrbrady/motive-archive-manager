d20ef51a460cf0688f49f3d49d2bb28cb3e4c9fe
andrewrbrady <28665474+andrewrbrady@users.noreply.github.com>
2025-01-25 21:38:08 -0600

    fix: revert to standard mongodb srv connection

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -4,23 +4,9 @@ if (!process.env.MONGODB_URI) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
 
-// Convert srv connection string to direct connection
-const uri = process.env.MONGODB_URI.replace(
-  "mongodb+srv://",
-  "mongodb://"
-).replace(
-  "/?",
-  ",motivearchive-shard-00-01.aaqps.mongodb.net:27017,motivearchive-shard-00-02.aaqps.mongodb.net:27017/?"
-);
+const uri = process.env.MONGODB_URI;
 
 const options = {
-  ssl: true,
-  tls: true,
-  tlsAllowInvalidCertificates: false,
-  tlsCAFile: undefined,
-  replicaSet: "atlas-5aqjui-shard-0",
-  authSource: "admin",
-  directConnection: false,
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
