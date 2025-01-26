2364a18bba84869cf1e523b0dbdea73f49f81187
andrewrbrady <28665474+andrewrbrady@users.noreply.github.com>
2025-01-25 21:24:44 -0600

    feat: add new image domains to next.config.js

--- next.config.js
+++ next.config.js
@@ -19,6 +19,16 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "sothebysmotorsport.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "d3sm3e0zl6r9d1.cloudfront.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "bringatrailer.com",
         pathname: "/**",
       },
@@ -40,6 +50,46 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "cdn.prod.website-files.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.millermotorcars.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "millermotorcars.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.chicagomotorcars.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "chicagomotorcars.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.dealerspike.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "automanager.blob.core.windows.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.mouse-motors.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "mouse-motors.com",
         pathname: "/**",
       },
     ],
